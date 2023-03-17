8eeca66c43932c1005b1267c519d1eb89c79f9cd
dmtrbch <bochvarovskidimitar@gmail.com>
2023-03-17 14:23:29 +0100

    use createPortal for creating a modal for the application

---
+++ src/Modal.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null); // I have a div with a modal id and I want to have this same div back every time and not creating new
+
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
+
+    return () => modalRoot.removeChild(elRef.current); // equal to componentWillUnmount for class components, cleanup
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
