70d65f6f18218932de234cfe1a4bccc41d972f90
dmtrbch <bochvarovskidimitar@gmail.com>
2023-03-17 12:13:50 +0100

    refactoring useBreedList to use react query instead of useEffect

---
+++ src/fetchBreedList.js
@@ -0,0 +1,19 @@
+const fetchBreedList = async ({ queryKey }) => {
+  const animal = queryKey[1];
+
+  if (!animal) return [];
+
+  const apiRes = await fetch(
+    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+  );
+
+  if (!apiRes.ok) {
+    // if we use react query we must handle errors
+    throw new Error(`breeds/${animal} fetch not ok`);
+  }
+
+  // react query expects to return promise
+  return apiRes.json(); // this returns promise
+};
+
+export default fetchBreedList;
